9e4f37a79bf2c4ca9973b5c5f86847df92745992
Nesline DAlmeida <neslinedalmeida@gmail.com>
2023-08-12 21:41:21 +0530

    Done

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -51,6 +51,12 @@ public class Car extends Vehicle {
         return isManual;
     }
 
-
+    public int getCurrentGear() {
+        return currentGear;
     }
+
+    public int getSeats() {
+        return seats;
+    }
+}
 
